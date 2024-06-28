f8c8181472fc7b3270b9f096ac9daf6f9cc34c1f
oooowb <oooo940212@gmail.com>
2024-06-28 16:20:02 +0900

    day11
    게시물 인터셉터 로그인화면으로 연결

--- src/main/java/com/workwave/interceptor/BoardInterceptor.java
+++ src/main/java/com/workwave/interceptor/BoardInterceptor.java
@@ -17,7 +17,7 @@ public class BoardInterceptor implements HandlerInterceptor {
         log.debug("after login interceptor execute!");
         if(!LoginUtil.isLoggedIn(request.getSession())) {        //로그인이 되어있으면 못들어가게
             log.info("로그인이 필요합니다.:{}",request.getRequestURI());
-            response.sendRedirect("/join");  //리다이렉트로 홈으로 보냄
+            response.sendRedirect("/login");  //리다이렉트로 홈으로 보냄
 //            response.sendError(403);
             return false;   //true일 경우, 컨트롤러 진입 허용 / false는 진입 차단
         }
