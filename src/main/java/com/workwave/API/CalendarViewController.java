56929275affe91f0a46eada4184d4f30fed2ea69
Maybaba <sinyunjong@gmail.com>
2024-06-29 14:48:41 +0900

    # 일정 추가 가능

--- src/main/java/com/workwave/API/CalendarViewController.java
+++ src/main/java/com/workwave/API/CalendarViewController.java
@@ -26,29 +26,27 @@ public class CalendarViewController {
     private final CalendarService calendarService;
     private final ObjectMapper objectMapper;
 
-    갖고온 것
-    @GetMapping("/view/{userId}")
-    public String viewCalendar(@PathVariable("userId") String userId, Model model, HttpSession session) {
-        List<AllMyCalendarEventDto> myCalendarEvents = calendarService.getMyEventsForMonth(userId);
-        try {
-            System.out.println("isLoggedIn: " + LoginUtil.isLoggedIn(session));
-            System.out.println("getLoggedIN: " + LoginUtil.getLoggedInUser(session));
-
-            String mycalEventsJson = objectMapper.writeValueAsString(myCalendarEvents);
-            model.addAttribute("mycalEvents", mycalEventsJson.replace("'", "\\'"));
-            // formattedDate 설정
-            String formattedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            model.addAttribute("formattedDate", formattedDate);
-            // 유저 이름 설정
-            String userName = myCalendarEvents.isEmpty() ? "Unknown User" : myCalendarEvents.get(0).getUserName();
-            model.addAttribute("userName", userName);
-
-        } catch (Exception e) {
-            log.error("Error converting events to JSON", e);
-        }
-        return "schedule/calendar/calendar";
-
-
+//    갖고온 것
+//    @GetMapping("/view/{userId}")
+//    public String viewCalendar(@PathVariable("userId") String userId, Model model, HttpSession session) {
+//        List<AllMyCalendarEventDto> myCalendarEvents = calendarService.getMyEventsForMonth(userId);
+//        try {
+//            System.out.println("isLoggedIn: " + LoginUtil.isLoggedIn(session));
+//            System.out.println("getLoggedIN: " + LoginUtil.getLoggedInUser(session));
+//
+//            String mycalEventsJson = objectMapper.writeValueAsString(myCalendarEvents);
+//            model.addAttribute("mycalEvents", mycalEventsJson.replace("'", "\\'"));
+//            // formattedDate 설정
+//            String formattedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//            model.addAttribute("formattedDate", formattedDate);
+//            // 유저 이름 설정
+//            String userName = myCalendarEvents.isEmpty() ? "Unknown User" : myCalendarEvents.get(0).getUserName();
+//            model.addAttribute("userName", userName);
+//
+//        } catch (Exception e) {
+//            log.error("Error converting events to JSON", e);
+//        }
+//        return "schedule/calendar/calendar";
 
     @GetMapping("/view")
     public String viewCalendar(Model model, HttpSession session) {
