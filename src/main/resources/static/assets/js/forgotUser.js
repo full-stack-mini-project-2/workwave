56dcf51a19e34bdb33cc69565be49841fa69e8f1
ckk914 <ckk9114@naver.com>
2024-07-08 12:09:26 +0900

    # ui 비밀번호 찾기 - 디자인 통일화

--- src/main/resources/static/assets/js/forgotUser.js
+++ src/main/resources/static/assets/js/forgotUser.js
@@ -12,16 +12,21 @@ const handleInputChange = debounce((e) => {
     if (result.message !== undefined) {
       if (result.message === "아이디가 중복되었습니다.") {
         $errorMessage.innerHTML =
+          '<i class="fas fa-check-circle" style="color: forestgreen;"></i>' +
           "아이디가 확인되었습니다.\n 찾기를 진행하세요";
         $errorMessage.style.color = "forestgreen";
         $forgotSubmit.disabled = false;
       } else {
-        $errorMessage.innerHTML = result.message;
+        $errorMessage.innerHTML =
+          '<i class="fas fa-exclamation-circle" style="color: #ff7f00;"></i> ' +
+          result.message;
         $errorMessage.style.color = "#ff7f00";
         $forgotSubmit.disabled = true;
       }
     } else {
-      $errorMessage.innerHTML = "아이디가 존재하지 않습니다.";
+      $errorMessage.innerHTML =
+        '<i class="fas fa-exclamation-circle" style="color: #ff7f00;"></i> ' +
+        "아이디가 존재하지 않습니다.";
       $errorMessage.style.color = "#ff7f00";
       $forgotSubmit.disabled = true;
     }
